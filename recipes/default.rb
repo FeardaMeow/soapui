50cf97d6554c041036a55b45a18f5b18773347ad
FeardaMeow <shwang1121@gmail.com>
2015-04-14 09:53:32 -0700

    Update to recipes default.rb
    Fixed missing "

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,7 +6,7 @@ remote file "#{node['soapui']['path']}#{node['soapui']['tarname']}" do
 end
 
 execute 'extract_some_tar' do
-	command "tar xzvf #{node['soapui']['tarname']}
+	command "tar xzvf #{node['soapui']['tarname']}"
 	cwd node['soapui']['path']
 	not_if ( File.exists?(node['soapui']['extract']) )
 end
