eb988fea52320240d6a94196cb673616e54f6500
FeardaMeow <shwang1121@gmail.com>
2015-04-14 09:46:59 -0700

    Update to attribute default.rb
    Fixed source

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['soapui'][source'] = 'http://sourceforge.net/projects/soapui/files/soapui/5.0.0/SoapUI-5.0.0-linux-bin.tar.gz/download'
+default['soapui'][source'] = '"http://sourceforge.net/projects/soapui/files/soapui/5.0.0/SoapUI-5.0.0-linux-bin.tar.gz/download"'
 default['soapui']['path'] = '/home/ubuntu/'
 default['soapui']['tarname'] = 'SoapUI-5.0.0-linux-bin.tar.gz'
 default['soapui']['extract'] = '/home/ubuntu/SoapUI-5.0.0'
